243316cbc7d8b3855b5c594ff35a480a4c2c416f
Roger <rdpender3@gmail.com>
2019-02-05 15:30:42 -0800

    messed with spacing a bit

--- main.rb
+++ main.rb
@@ -24,13 +24,14 @@ loop do
   puts 'Do you want to lookup an area code based on a city name? (Y/N)'
   answer = gets.chomp.downcase
   break if answer != 'y'
-  puts "Which city do you want the area code for?"  
+
+  puts 'Which city do you want the area code for?'
   puts get_city_names(dial_book)
-  puts "Enter your chosen city"
+  puts 'Enter your chosen city'
   city = gets.chomp
   if dial_book.include?(city)
     puts "The are code for #{city} is #{get_area_code(dial_book, city)}"
   else
-    puts "You have entered a city that is not in the directory"
+    puts 'You have entered a city that is not in the directory'
   end
 end
